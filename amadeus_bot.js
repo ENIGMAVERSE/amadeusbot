605cb7f93bbd19cbda5da4436d243e7049d6e654
ENIGMAVERSE <70476036+ENIGMAVERSE@users.noreply.github.com>
2021-07-17 21:52:22 +0530

    Update amadeus_bot.js

--- amadeus_bot.js
+++ amadeus_bot.js
@@ -1746,4 +1746,4 @@ function listCommand(arguments, receivedMessage) {
 }
 
 //Logs into Discord
-client.login("NzQ4MDgwMDczMjU2NzMwNjQ1.X0YN3A.w8yuDF-51R1HAgo9511sAEn-Yc0")
+client.login("NzQ4MDgwMDczMjU2NzMwNjQ1.X0YN3A.vpoq5p6ASC6TD8AV2oMS8EXsz7o")
